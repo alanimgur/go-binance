5b10a27ba8898dd14dbbc1e2c7d5474cdacaf548
Alan Schaaf <alan@imgur.com>
2018-01-15 12:49:45 -0800

    8 precision

--- binance/account.go
+++ binance/account.go
@@ -12,10 +12,9 @@ import (
 
 // Get Basic Account Information
 func (b *Binance) GetAccountInfo() (account Account, err error) {
+	reqURL := fmt.Sprintf("api/v3/account")
 
-	reqUrl := fmt.Sprintf("api/v3/account")
-
-	_, err = b.client.do("GET", reqUrl, "", true, &account)
+	_, err = b.client.do("GET", reqURL, "", true, &account)
 	if err != nil {
 		return
 	}
@@ -25,11 +24,10 @@ func (b *Binance) GetAccountInfo() (account Account, err error) {
 
 // Filter Basic Account Information To Retrieve Current Holdings
 func (b *Binance) GetPositions() (positions []Balance, err error) {
-
-	reqUrl := fmt.Sprintf("api/v3/account")
+	reqURL := fmt.Sprintf("api/v3/account")
 	account := Account{}
 
-	_, err = b.client.do("GET", reqUrl, "", true, &account)
+	_, err = b.client.do("GET", reqURL, "", true, &account)
 	if err != nil {
 		return
 	}
@@ -49,9 +47,13 @@ func (b *Binance) GetPositions() (positions []Balance, err error) {
 
 // Place a Limit Order
 func (b *Binance) PlaceLimitOrder(l LimitOrder) (res PlacedOrder, err error) {
-	reqUrl := fmt.Sprintf("api/v3/order?symbol=%s&side=%s&type=%s&quantity=%f&price=%f&recvWindow=%d", l.Symbol, l.Side, l.Type, l.Quantity, l.Price, l.RecvWindow)
+	reqURL := fmt.Sprintf("api/v3/order?symbol=%s&side=%s&type=%s&quantity=%.8f&price=%.8f&recvWindow=%d", l.Symbol, l.Side, l.Type, l.Quantity, l.Price, l.RecvWindow)
 
-	_, err = b.client.do("POST", reqUrl, "", true, &res)
+	if l.Type == "LIMIT" {
+		reqURL = fmt.Sprintf("%s&timeInForce=%s", reqURL, l.TimeInForce)
+	}
+
+	_, err = b.client.do("POST", reqURL, "", true, &res)
 	if err != nil {
 		return
 	}
@@ -61,15 +63,14 @@ func (b *Binance) PlaceLimitOrder(l LimitOrder) (res PlacedOrder, err error) {
 
 // Place a Market Order
 func (b *Binance) PlaceMarketOrder(m MarketOrder) (res PlacedOrder, err error) {
-
 	err = m.ValidateMarketOrder()
 	if err != nil {
 		return
 	}
 
-	reqUrl := fmt.Sprintf("api/v3/order?symbol=%s&side=%s&type=%s&quantity=%f&recvWindow=%d", m.Symbol, m.Side, m.Type, m.Quantity, m.RecvWindow)
+	reqURL := fmt.Sprintf("api/v3/order?symbol=%s&side=%s&type=%s&quantity=%.8f&recvWindow=%d", m.Symbol, m.Side, m.Type, m.Quantity, m.RecvWindow)
 
-	_, err = b.client.do("POST", reqUrl, "", true, &res)
+	_, err = b.client.do("POST", reqURL, "", true, &res)
 	if err != nil {
 		return
 	}
@@ -85,9 +86,9 @@ func (b *Binance) CancelOrder(query OrderQuery) (order CanceledOrder, err error)
 		return
 	}
 
-	reqUrl := fmt.Sprintf("api/v3/order?symbol=%s&orderId=%d&recvWindow=%d", query.Symbol, query.OrderId, query.RecvWindow)
+	reqURL := fmt.Sprintf("api/v3/order?symbol=%s&orderId=%d&recvWindow=%d", query.Symbol, query.OrderId, query.RecvWindow)
 
-	_, err = b.client.do("DELETE", reqUrl, "", true, &order)
+	_, err = b.client.do("DELETE", reqURL, "", true, &order)
 	if err != nil {
 		return
 	}
@@ -97,15 +98,14 @@ func (b *Binance) CancelOrder(query OrderQuery) (order CanceledOrder, err error)
 
 // Check the Status of an Order
 func (b *Binance) CheckOrder(query OrderQuery) (status OrderStatus, err error) {
-
 	err = query.ValidateOrderQuery()
 	if err != nil {
 		return
 	}
 
-	reqUrl := fmt.Sprintf("api/v3/order?symbol=%s&orderId=%d&origClientOrderId=%s&recvWindow=%d", query.Symbol, query.OrderId, query.RecvWindow)
+	reqURL := fmt.Sprintf("api/v3/order?symbol=%s&orderId=%d&origClientOrderId=%s&recvWindow=%d", query.Symbol, query.OrderId, query.RecvWindow)
 
-	_, err = b.client.do("GET", reqUrl, "", true, &status)
+	_, err = b.client.do("GET", reqURL, "", true, &status)
 	if err != nil {
 		return
 	}
@@ -126,13 +126,12 @@ func (b *Binance) GetAllOpenOrders() (orders []OrderStatus, err error) {
 
 // Retrieve All Open Orders for a given symbol
 func (b *Binance) GetOpenOrders(query OpenOrdersQuery) (orders []OrderStatus, err error) {
-
 	err = query.ValidateOpenOrdersQuery()
 	if err != nil {
 		return
 	}
-	reqUrl := fmt.Sprintf("api/v3/openOrders?symbol=%s&recvWindow=%d", query.Symbol, query.RecvWindow)
-	_, err = b.client.do("GET", reqUrl, "", true, &orders)
+	reqURL := fmt.Sprintf("api/v3/openOrders?symbol=%s&recvWindow=%d", query.Symbol, query.RecvWindow)
+	_, err = b.client.do("GET", reqURL, "", true, &orders)
 	if err != nil {
 		return
 	}
@@ -142,10 +141,9 @@ func (b *Binance) GetOpenOrders(query OpenOrdersQuery) (orders []OrderStatus, er
 
 // Retrieves all trades
 func (b *Binance) GetTrades(symbol string) (trades []Trade, err error) {
+	reqURL := fmt.Sprintf("api/v3/myTrades?symbol=%s", symbol)
 
-	reqUrl := fmt.Sprintf("api/v3/myTrades?symbol=%s", symbol)
-
-	_, err = b.client.do("GET", reqUrl, "", true, &trades)
+	_, err = b.client.do("GET", reqURL, "", true, &trades)
 
 	if err != nil {
 		return
@@ -156,10 +154,9 @@ func (b *Binance) GetTrades(symbol string) (trades []Trade, err error) {
 //
 // Retrieves all withdrawals
 func (b *Binance) GetWithdrawHistory() (withdraws WithdrawList, err error) {
+	reqURL := fmt.Sprintf("wapi/v3/withdrawHistory.html")
 
-	reqUrl := fmt.Sprintf("wapi/v3/withdrawHistory.html")
-
-	_, err = b.client.do("GET", reqUrl, "", true, &withdraws)
+	_, err = b.client.do("GET", reqURL, "", true, &withdraws)
 	if err != nil {
 		return
 	}
@@ -169,10 +166,9 @@ func (b *Binance) GetWithdrawHistory() (withdraws WithdrawList, err error) {
 //
 // Retrieves all deposits
 func (b *Binance) GetDepositHistory() (deposits DepositList, err error) {
+	reqURL := fmt.Sprintf("wapi/v3/depositHistory.html")
 
-	reqUrl := fmt.Sprintf("wapi/v3/depositHistory.html")
-
-	_, err = b.client.do("GET", reqUrl, "", true, &deposits)
+	_, err = b.client.do("GET", reqURL, "", true, &deposits)
 	if err != nil {
 		return
 	}
